bbb7a01c108d9260685a0d8507e24c2b2b6095f9
joilsomichalski <joilsomichalski27@gmail.com>
2026-04-27 21:03:51 -0300

    Stopwatch-main

---
+++ script.js
@@ -0,0 +1,39 @@
+let number = 0
+let cron
+let h1 = document.querySelector('h1')
+
+let btnStart = document.getElementById('btnStart')
+let btnStop = document.getElementById('btnStop')
+
+btnStop.style.display = 'none'
+
+function start() {
+    clearInterval(cron)
+
+    number = 0
+
+    btnStart.style.display = 'none'
+    btnStop.style.display = 'inline-block'
+
+    cron = setInterval(function() {
+        number++
+
+        let horas = Math.floor(number / 3600)
+        let minutos = Math.floor((number % 3600) / 60)
+        let segundos = number % 60
+
+        if (horas < 10) horas = '0' + horas
+        if (minutos < 10) minutos = '0' + minutos
+        if (segundos < 10) segundos = '0' + segundos
+
+        h1.innerHTML = horas + ':' + minutos + ':' + segundos
+    }, 10)
+}
+
+function stop() {
+    clearInterval(cron)
+
+    btnStop.style.display = 'none'
+    btnStart.style.display = 'inline-block'    
+
+}
